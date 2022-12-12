ce4702baed98eb9fa1f3936584e775aaff1d914b
abhishekgw5 <111060091+abhishekgw5@users.noreply.github.com>
2022-12-12 08:30:12 +0530

    Update factrecursive.sh

--- factrecursive.sh
+++ factrecursive.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-
-echo "Enter a number"
-read num
-factorial=1
-for((i=2;i<=num;i++)){
-  factorial=$((factorial * i))
+factorial(){
+    product=$1
+    if((product <= 2)); then
+        echo $product
+    else
+        f=$((product -1))
+        f=$(factorial $f)
+        f=$((f*product))
+        echo $f
+fi
 }
-echo $factorial
+
+echo "Enter the number:"
+read num
+if((num == 0)); then
+    echo 1
+else
+    factorial $num
+fi
